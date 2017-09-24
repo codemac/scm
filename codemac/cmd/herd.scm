5f51e4db8bb4ff7b04adcf5cb3518dfa81ceb8b8
Jeff Mickey <j@codemac.net>
2017-09-24 12:19:10 -0700

    Update herd to pass args correctly

--- codemac/cmd/herd.scm
+++ codemac/cmd/herd.scm
@@ -5,4 +5,5 @@ exec guile -e "(@@ (codemac cmd herd) main)" -s "$0" "$@"
 (define-module (codemac cmd herd)
   #:use-module (shepherd scripts herd))
 
-(apply main (cdr (command-line)))
+(define (main . args)
+  (apply (@ (shepherd scripts herd) main) (cdr (command-line))))
