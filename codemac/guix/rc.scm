67054b66051ca3b94332a887e7e91b14a0e0d933
Jeff Mickey <j@codemac.net>
2021-02-16 23:43:54 -0800

    rc updates

--- codemac/guix/rc.scm
+++ codemac/guix/rc.scm
@@ -6,7 +6,6 @@
   #:use-module (gnu packages bison)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages crates-graphics)
-  #:use-module (gnu packages crates-io)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages documentation)
   #:use-module (gnu packages groff)
@@ -18,15 +17,10 @@
   #:use-module (gnu packages pcre)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages readline)
-  #:use-module (gnu packages rust)
-  #:use-module (gnu packages rust-apps)
   #:use-module (gnu packages scheme)
   #:use-module (gnu packages tls)
   #:use-module (gnu packages version-control)
-  #:use-module (gnu packages xorg)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system trivial)
   #:use-module (guix download)
@@ -36,41 +30,41 @@
   #:use-module (guix utils))
 
 (package
-    (name "rc")
-    (version "ceb59bb2a644f4ebc1645fe15f1063029579fa7c")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/rakitzis/rc")
-                    (commit version)))
-              (sha256
-               (base32
-                "0vj1h4pcg13vxsiydmmk87dr2sra9h4gwx0c4q6fjsiw4in78rrd"))
-              (file-name (git-file-name name version))))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:configure-flags
-       '("--with-edit=gnu")
-       #:phases
-       (modify-phases %standard-phases
-         (add-before 'bootstrap 'patch-trip.rc
-          (lambda _
-            (substitute* "trip.rc"
-              (("/bin/pwd") (which "pwd"))
-              (("/bin/sh")  (which "sh"))
-              (("/bin/rm")  (which "rm"))
-              (("/bin\\)")  (string-append (dirname (which "rm")) ")")))
-            #t)))))
-    (inputs `(("readline" ,readline)
-              ("perl" ,perl)))
-    (native-inputs `(("autoconf" ,autoconf)
-                     ("automake" ,automake)
-                     ("libtool" ,libtool)
-                     ("pkg-config" ,pkg-config)))
-    (synopsis "Alternative implementation of the rc shell by Byron Rakitzis")
-    (description
-     "This is a reimplementation by Byron Rakitzis of the Plan 9 shell.  It
+ (name "rc")
+ (version "ceb59bb2a644f4ebc1645fe15f1063029579fa7c")
+ (source (origin
+          (method git-fetch)
+          (uri (git-reference
+                (url "https://github.com/rakitzis/rc")
+                (commit version)))
+          (sha256
+           (base32
+            "0vj1h4pcg13vxsiydmmk87dr2sra9h4gwx0c4q6fjsiw4in78rrd"))
+          (file-name (git-file-name name version))))
+ (build-system gnu-build-system)
+ (arguments
+  `(#:configure-flags
+    '("--with-edit=gnu")
+    #:phases
+    (modify-phases %standard-phases
+		   (add-before 'bootstrap 'patch-trip.rc
+			       (lambda _
+				 (substitute* "trip.rc"
+					      (("/bin/pwd") (which "pwd"))
+					      (("/bin/sh")  (which "sh"))
+					      (("/bin/rm")  (which "rm"))
+					      (("/bin\\)")  (string-append (dirname (which "rm")) ")")))
+				 #t)))))
+ (inputs `(("readline" ,readline)
+           ("perl" ,perl)))
+ (native-inputs `(("autoconf" ,autoconf)
+                  ("automake" ,automake)
+                  ("libtool" ,libtool)
+                  ("pkg-config" ,pkg-config)))
+ (synopsis "Alternative implementation of the rc shell by Byron Rakitzis")
+ (description
+  "This is a reimplementation by Byron Rakitzis of the Plan 9 shell.  It
 has a small feature set similar to a traditional Bourne shell.")
-    (home-page "https://github.com/rakitzis/rc")
-    (license license:zlib)))
+ (home-page "https://github.com/rakitzis/rc")
+ (license license:zlib))
 
