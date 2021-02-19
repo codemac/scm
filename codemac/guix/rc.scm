a4e91e2edf2d7021b35c78626013d25e1f043743
Jeff Mickey <j@codemac.net>
2021-02-19 12:26:37 -0800

    better hash and name

--- codemac/guix/rc.scm
+++ codemac/guix/rc.scm
@@ -1,13 +1,13 @@
-(define-module (rc)
+(define-module (codemac guix rc)
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (gnu packages)
   #:use-module (gnu packages shells)
   #:use-module (guix utils))
 
-(define-public rc-master
+(define-public rc-byron
  (package (inherit rc)
- (name "rc-master")
+ (name "rc-byron")
  (version "ceb59bb2a644f4ebc1645fe15f1063029579fa7c")
  (source (origin
           (method git-fetch)
@@ -16,6 +16,6 @@
                 (commit version)))
           (sha256
            (base32
-            "0vj1h4pcg13vxsiydmmk87dr2sra9h4gwx0c4q6fjsiw4in78rrd"))
+            "1q0c6dq5f8aapcq0gg5vf9fs1ibcjij433vvyrjg0lx6icbz75sp"))
           (file-name (git-file-name name version))))))
 
